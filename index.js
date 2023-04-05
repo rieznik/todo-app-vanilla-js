30b8e13ca8d791b6d3c13c92919a069186f5b68f
Kateryna Rieznik <rieznik.k@gmail.com>
2023-04-05 18:06:51 +0300

    add code from the workshop

---
+++ index.js
@@ -0,0 +1,53 @@
+import { renderList } from './js/renderList.js';
+import { formListener } from './js/formListener.js';
+import { saveList, loadList } from './js/ls.js';
+import { listClickListener } from './js/listClickListener.js';
+import { formChangeListener } from './js/formChangeListener.js';
+import { byName } from './js/filters.js';
+
+// Done ✅
+// Create a new task
+// by clicking on the button
+// by pressing enter
+// Delete a task
+// Search tasks by name
+
+// TODO 🔲
+// Mark a task as completed (1/2)
+// Delete all completed tasks
+// Filter tasks by completed and uncompleted (different lists)
+
+// Save data to localStorage
+
+const startApp = () => {
+  console.log('Workshop1 startApp');
+
+  const tasksList = loadList();
+
+  renderList(tasksList);
+
+  const updateList = () => {
+    saveList(tasksList);
+    renderList(tasksList);
+  };
+
+  formListener((newTask) => {
+    tasksList.push({
+      name: newTask,
+      completed: false,
+    });
+    updateList();
+  });
+
+  listClickListener((index) => {
+    tasksList.splice(index, 1);
+    updateList();
+  });
+
+  formChangeListener((text) => {
+    const filteredTasks = tasksList.filter(byName(text));
+    renderList(filteredTasks);
+  });
+};
+
+document.addEventListener('DOMContentLoaded', startApp);
